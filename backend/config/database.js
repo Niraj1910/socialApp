4355870edbadca7a62afd0d7bbb963d29438d0bd
Niraj Shaw <nirazshaw156@gmail.com>
2023-12-05 18:10:43 +0530

    bakend updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
+// Set the 'strictQuery' option to false to prepare for the change in Mongoose 7
+mongoose.set("strictQuery", false);
+
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
